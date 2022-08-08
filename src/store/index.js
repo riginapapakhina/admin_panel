35859112ad2aa84b97ccd8f7ce7b0a7a121b9f38
Rigina <papakhina.rigina@yandex.ru>
2022-08-08 15:42:22 +0300

    middleware added

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,16 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+const stringMiddleware= ()=>(next)=>(action)=>{
+  if(typeof action ==='string'){
+    return next({
+      type:action
+    })
+  }
+  return next(action)
+}
+
 
 const enhancer=(createStore)=>(...args)=>{
   const store = createStore(...args);
@@ -18,10 +28,10 @@ const enhancer=(createStore)=>(...args)=>{
 
 const store = createStore( 
         combineReducers({heroes, filters}),
-        compose(
-          enhancer,
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        ))
+        compose(applyMiddleware(stringMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+       
+        )
    
 export default store;
 
