3cbaaa196bafe8a9473ad1736aa484c5122781ba
Rigina <papakhina.rigina@yandex.ru>
2022-08-09 15:13:51 +0300

    error in heroesSlice fixed

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,68 +1,51 @@
-import { createSlice, createAsyncThunk, createEntityAdapter, createSelector } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import {useHttp} from '../../hooks/http.hook';
 
-const heroesAdapter = createEntityAdapter();
-
-const initialState =heroesAdapter.getInitialState({
-  heroesLoadingStatus:'idle'
-})
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: 'idle'
+}
 
 export const fetchHeroes = createAsyncThunk(
     'heroes/fetchHeroes',
-    async ()=>{
-      const {request} = useHttp();
-      return await request("http://localhost:3001/heroes")
+    async () => {
+        const {request} = useHttp();
+        return await request("http://localhost:3001/heroes");
     }
 );
 
-const heroesSlice=createSlice({
-    name:'heroes',
+const heroesSlice = createSlice({
+    name: 'heroes',
     initialState,
-    reducers:{
-      
-    heroCreated: (state, action) => {
-        heroesAdapter.addOne(state, action.payload);
+    reducers: {
+        heroCreated: (state, action) => {
+            state.heroes.push(action.payload);
+        },
+        heroDeleted: (state, action) => {
+            state.heroes = state.heroes.filter(item => item.id !== action.payload);
+        }
     },
-    heroDeleted: (state, action) => {
-        heroesAdapter.removeOne(state, action.payload);
-    }
-    },
-    extraReducers: (builder)=>{
-      builder
-      .addCase(fetchHeroes.pending,state=>{state.heroesLoadingStatus='loading'})
-      .addCase(fetchHeroes.fulfilled,(state, action) => {
-        state.heroesLoadingStatus = 'idle';
-        heroesAdapter.setAll(state, action.payload)
-      })
-      .addCase(fetchHeroes.rejected, state => {
-        state.heroesLoadingStatus = 'error';
-    })
-    .addDefaultCase(()=>{})
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                state.heroesLoadingStatus = 'idle';
+                state.heroes = action.payload;
+            })
+            .addCase(fetchHeroes.rejected, state => {
+                state.heroesLoadingStatus = 'error';
+            })
+            .addDefaultCase(() => {})
     }
 });
-const{actions,reducer}= heroesSlice;
+
+const {actions, reducer} = heroesSlice;
+
 export default reducer;
-const {selectAll}=heroesAdapter.getSelectors(state=>state.heroes)
-
-
-const HeroesList = () => {
-
-  export const filteredHeroesSelector = createSelector(
-      (state) => state.filters.activeFilter,
-      selectAll,
-      (filter, heroes) => {
-          if (filter === 'all') {
-              return heroes;
-          } else {
-              return heroes.filter(item => item.element === filter);
-          }
-      }
-  )}
-
 export const {
-  heroesFetching,
-  heroesFetched,
-  heroesFetchingError,
-  heroCreated,
-  heroDeleted
-}=actions;
+    heroesFetching,
+    heroesFetched,
+    heroesFetchingError,
+    heroCreated,
+    heroDeleted
+} = actions;
