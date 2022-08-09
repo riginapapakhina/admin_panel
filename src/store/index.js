181bec24bdc8942841ff416ca0b288af0496fbd8
Rigina <papakhina.rigina@yandex.ru>
2022-08-09 12:54:56 +0300

    same for filters added

--- src/store/index.js
+++ src/store/index.js
@@ -1,46 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../components/heroesList/heroesSlice';
-import filters from '../reducers/filters';
+import filters from '../components/heroesFilters/filtersSlice';
 
-const stringMiddleware= ()=>(next)=>(action)=>{
-  if(typeof action ==='string'){
-    return next({
-      type:action
-    })
-  }
-  return next(action)
-}
-
-
-const enhancer=(createStore)=>(...args)=>{
-  const store = createStore(...args);
-  const oldDispatch=store.dispatch;
-  store.dispatch=(action)=>{
-    if(typeof action ==='string'){
-      return oldDispatch({
-        type:action
-      })
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
     }
-    return oldDispatch(action)
-  }
-  return store;
-}
+    return next(action)
+};
 
-// const store = createStore( 
-//         combineReducers({heroes, filters}),
-//         compose(applyMiddleware(ReduxThunk,stringMiddleware),
-//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-       
-//         )
-   
-const store=configureStore({
-  reducer:{heroes, filters},
-  middleware:getDefaultMiddleware=>getDefaultMiddleware().concat(stringMiddleware),
-  devTools: process.env.NODE_ENV !== 'production',
-
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
 })
 
 export default store;
-
-
-
